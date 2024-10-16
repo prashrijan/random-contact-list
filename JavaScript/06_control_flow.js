08a4c5244c52f9ee7df761702ae6dbf3ac2af341
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 23:37:34 +1100

    else if

--- JavaScript/06_control_flow.js
+++ JavaScript/06_control_flow.js
@@ -38,3 +38,21 @@
 // }
 
 
+let grade = 10
+
+if(grade > 90 && grade <= 100){
+    console.log("A+")
+}else if(grade > 80 && grade <= 90){
+    console.log("A");
+}else if(grade > 70 && grade <= 80){
+    console.log("B+");
+}else if(grade > 60 && grade <= 70){
+    console.log("B");
+}else if(grade > 50 && grade <= 60){
+    console.log("C+");
+}else if(grade > 40 && grade <= 50){
+    console.log("C");
+}else{
+    console.log("You Failed");
+    
+}

4b05f35b8ceed9ea89768ca9b8834aa48611498d
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 12:29:37 +1100

    JS varialbes

--- JavaScript/01_varibales.js
+++ JavaScript/01_varibales.js
@@ -3,4 +3,12 @@ name = "Prashrijan Shrestha"
 
 let add = "Sydney"
 
+// age = 20 automatically considered var
+
+const age = 20
+// age = 1 not allowed
+
+console.log(age);
+
+
 console.log(name, add);

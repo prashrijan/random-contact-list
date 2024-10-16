882851436f8a2a58fe137bd2c2421e0c661e010e
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-17 00:35:50 +1100

    array

--- JavaScript/08_array.js
+++ JavaScript/08_array.js
@@ -1,3 +1,30 @@
-const arr = ["Hello", 123, "world", null, undefined, {}, [123, "Prash"], () => {}]
+// const arr = ["Hello", 123, "world", null, undefined, {}, [123, "Prash"], () => {}]
 
-console.log(arr);
+// console.log(arr);
+
+
+const avengers = ["Iron Man", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye"]
+
+console.log(avengers.length);
+
+console.log(avengers.push("Spiderman"));
+
+// let newAvengers = avengers.splice(2, 3)
+
+// console.log(newAvengers);
+
+let part = avengers.slice(2, 4)
+
+console.log(part);
+
+console.log(part.toString());
+
+
+console.log(avengers.sort());
+
+
+
+console.log(avengers);
+
+
+

849347df1774156c4b3498b5f85b4352f9fbdfb6
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 23:30:18 +1100

    control flow part 1

---
+++ JavaScript/06_control_flow.js
@@ -0,0 +1,32 @@
+// Truthy Values
+// 1, "something_inside", true, [], -2, {}, function(){}
+
+// Falsy Values
+// 0, "", false, NaN, null, undefined
+
+// if ("") {
+//     console.log("Hello World");
+// }
+
+// if (!sayHello) {
+//     console.log("Hello World Opposite");
+// }
+
+
+// function sayHello(){
+//     return "hello"
+// }
+
+
+const age = 10
+
+if(age >= 18 && age <=60 ){
+    console.log("You are an adult");
+}
+
+const isVip = true
+
+if(age >=18 || isVip){
+    console.log("Welcome to the party");
+    
+}

38616f8597a0650ec6b6d7a1c9151bd5c5721aee
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 12:35:51 +1100

    operators

---
+++ JavaScript/02_operators.js
@@ -0,0 +1,24 @@
+// Arithmetic Operators
+// Comparison Operartors
+// Logical Operators
+
+let a = 5
+let b = 6
+let c = b
+
+let add = a + b + c
+let sub = a - b - c
+
+
+// console.log(a++);
+// console.log(a);
+
+console.log(++a);
+console.log(a);
+
+
+
+// console.log(add);
+// console.log(sub);
+
+

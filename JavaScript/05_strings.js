0ee084b3fcb182aa0a81a52c88b0e535057d52cc
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 23:01:35 +1100

    string manipulation part 2

--- JavaScript/05_strings.js
+++ JavaScript/05_strings.js
@@ -1,19 +1,29 @@
 const str = "Hello world, my name is \"Prashrijan Shrestha\""
 
-console.log(str);
-console.log(str.length);
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str[str.length - 2]);
+// console.log(str);
+// console.log(str.length);
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str[str.length - 2]);
 
 
-console.log(str.charAt(20));
+// console.log(str.charAt(20));
 
-console.log(str.indexOf("P"));
+// console.log(str.indexOf("P"));
 
-console.log(str.replace("world", "guys"));
+// console.log(str.replace("world", "guys"));
 
 
+// let strPart = str.slice(2, 5)
+// let strPart = str.slice(2)
+// let strPart = str.slice(-5)
+
+// console.log(strPart);
+
+const val = str.includes("Prashrijan")
+console.log(val);
+
+console.log(str.search("Shrestha"));
 
 
 

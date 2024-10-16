efecf0c31c01f14084e43bd5efac2a1c610b4ff6
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 22:45:11 +1100

    number manipulations

---
+++ JavaScript/04_number.js
@@ -0,0 +1,21 @@
+// Safe number range -2^53-1 to 2^53-1
+
+// const num = 450_528_184;
+
+// console.log(num);
+
+// Type Coercion
+
+const num = "5aaa"
+// const val = +num
+const val = parseInt(num)
+
+console.log(val);
+
+
+const num2 = "30.5aa5.6"
+
+const val2 = parseFloat(num2)
+console.log(val2);
+
+

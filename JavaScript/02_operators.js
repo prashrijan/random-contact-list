06724eaa62812a2fbff9b56788641f5f38834c07
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 12:43:44 +1100

    math object

--- JavaScript/02_operators.js
+++ JavaScript/02_operators.js
@@ -13,12 +13,31 @@ let sub = a - b - c
 // console.log(a++);
 // console.log(a);
 
-console.log(++a);
-console.log(a);
-
-
+// console.log(++a);
+// console.log(a);
 
 // console.log(add);
 // console.log(sub);
 
 
+// Math Object
+
+const pi = Math.PI
+
+// console.log(Math.round(3.14567889))
+
+// console.log(pi);
+
+
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9));
+
+
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 9, 9, 8, 7));
+// console.log(Math.max(1, 2, 3, 4, 5, 6, 9, 9, 8, 7));
+
+
+
+let random = Math.floor((Math.random() * 100) + 1)
+
+console.log(random);

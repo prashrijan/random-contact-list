75a7de2deef0c83dda86f3dbe32ffbbaae20431a
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-16 22:57:02 +1100

    string manipulation part 1

---
+++ JavaScript/05_strings.js
@@ -0,0 +1,20 @@
+const str = "Hello world, my name is \"Prashrijan Shrestha\""
+
+console.log(str);
+console.log(str.length);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str[str.length - 2]);
+
+
+console.log(str.charAt(20));
+
+console.log(str.indexOf("P"));
+
+console.log(str.replace("world", "guys"));
+
+
+
+
+
+// console.log("4" + "5" + 6 + 4);

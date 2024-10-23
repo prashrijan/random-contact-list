bb8f3b59035cadcc3d95fbd01e5224dba01858bb
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-23 13:52:39 +1100

    loops

---
+++ JavaScript/09_loops.js
@@ -0,0 +1,30 @@
+// for (let  i = 0;  i < 10; i++) {
+//         if(i === 5){
+//             continue
+//         }
+
+//         if(i === 8){
+//             break
+//         }
+
+//         console.log(i);
+    
+// }
+
+
+// let nums = []
+
+
+// for(let i = 0; i < 10; i++){
+//     let randomNumber = Math.floor((Math.random() * 10) + 1)
+
+//     if(nums.includes(randomNumber)){
+//         continue
+//     }
+//     console.log(randomNumber);
+    
+
+//     nums.push(randomNumber)
+// }
+
+// console.log(nums);

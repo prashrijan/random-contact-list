929b5055fe0b652dffb64bee130daaec6d36303a
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-24 11:40:34 +1100

    array new fill

--- JavaScript/08_array.js
+++ JavaScript/08_array.js
@@ -1,30 +1,33 @@
-// const arr = ["Hello", 123, "world", null, undefined, {}, [123, "Prash"], () => {}]
+// // const arr = ["Hello", 123, "world", null, undefined, {}, [123, "Prash"], () => {}]
 
-// console.log(arr);
-
-
-const avengers = ["Iron Man", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye"]
-
-console.log(avengers.length);
-
-console.log(avengers.push("Spiderman"));
-
-// let newAvengers = avengers.splice(2, 3)
-
-// console.log(newAvengers);
-
-let part = avengers.slice(2, 4)
-
-console.log(part);
-
-console.log(part.toString());
+// // console.log(arr);
 
 
-console.log(avengers.sort());
+// const avengers = ["Iron Man", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye"]
+
+// console.log(avengers.length);
+
+// console.log(avengers.push("Spiderman"));
+
+// // let newAvengers = avengers.splice(2, 3)
+
+// // console.log(newAvengers);
+
+// let part = avengers.slice(2, 4)
+
+// console.log(part);
+
+// console.log(part.toString());
+
+
+// console.log(avengers.sort());
 
 
 
-console.log(avengers);
+// console.log(avengers);
 
+let myArr = new Array(10).fill(null)
+
+console.log(myArr);
 
 

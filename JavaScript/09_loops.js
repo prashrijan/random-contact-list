cd2d0c1d6e8929f8b0bca18525e866e06d9750bd
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-24 11:37:32 +1100

    includes and find in array

--- JavaScript/09_loops.js
+++ JavaScript/09_loops.js
@@ -72,11 +72,11 @@ const fruits = ["apple", "banana", "mango", "orange"]
 
 
 // Reduce Method
-const cart = [1200, 200, 305, 320, 450, 435]
+// const cart = [1200, 200, 305, 320, 450, 435]
 
-const total = cart.reduce((sum, amount) => {
-    return sum + amount
-}, 0)
+// const total = cart.reduce((sum, amount) => {
+//     return sum + amount
+// }, 0)
 
 // First Iteration
 // // 0 + 1200 => 1200
@@ -93,10 +93,20 @@ const total = cart.reduce((sum, amount) => {
 // Every
 // does every item in the array passes the condition
 
-const nums1 = [1, 2, 3, 4, 5, 6, 7, "8", 9]
-const nums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const nums1 = [1, 2, 3, 4, 5, 6, 7, "8", 9]
+// const nums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-console.log(nums1.every(num => typeof(num) === 'number'))
-console.log(nums2.every(num => typeof(num) === 'number'))
+// console.log(nums1.every(num => typeof(num) === 'number'))
+// console.log(nums2.every(num => typeof(num) === 'number'))
 
 
+const cart = [1200, 200, 305, 320, 450, 435]
+
+// console.log(cart.includes(201))
+
+// find returns the first element that satisfies the conditionb
+
+
+let res = cart.find(item => item > 400)
+
+console.log(res);

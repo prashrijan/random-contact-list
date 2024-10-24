2d236bd43310719c8eb139959f80eb8d6035f89b
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-25 00:22:28 +1100

    obj

--- JavaScript/11_object.js
+++ JavaScript/11_object.js
@@ -1,4 +1,22 @@
-// 
+// // const person = {
+//     name: "Prashrijan Shrestha",
+//     age : 21,
+//     isAdult : () => this.age > 18,
+//     skills: ["HTML", "CSS", "JS"],
+// }
+
+// person.address = "89 Sydney"
+
+// const data = person.skills[1] 
+// const {name, age, skills, parents = ["ab, n"], ...restOf} = person
+
+// console.log(person);
+
+// console.log(data)
+// console.log(restOf)
+
+
+// console.log(name, age, skills, parents)
 
 // Shallow Copy / Deep Copy
 
@@ -6,3 +24,17 @@
 
 
 // Deep copy => copy whole obj
+
+const person = {
+    name: "Prashrijan Shrestha",
+    age : 21,
+    isAdult : () => this.age > 18,
+    skills: ["HTML", "CSS", "JS"],
+ }
+
+
+for(let key in person){
+    console.log(`${key}`);
+    
+}
+    

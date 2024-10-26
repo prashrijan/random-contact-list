73353b45b41b682614597903a20e4feed0d87f35
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-26 23:31:37 +1100

    date challenge 1

--- JavaScript/12_date.js
+++ JavaScript/12_date.js
@@ -10,4 +10,19 @@ let day = today.getDay()
 
 console.log(day);
 
+// Challenge 1
+// Given a person's date of birth, create a function that calculates and returns their age in years.
+
+let dob = "2003-07-07"
+
+let dobDate = new Date(dob)
+let dobYear = dobDate.getFullYear()
+
+let currentDate = new Date()
+let currYear = currentDate.getFullYear()
+
+let age = currYear - dobYear
+
+console.log(age)
+
 

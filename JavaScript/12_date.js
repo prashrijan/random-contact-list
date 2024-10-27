01597abf81e0369db44efbd0f1c01916b17cae24
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-27 21:58:12 +1100

    Date manipulation

--- JavaScript/12_date.js
+++ JavaScript/12_date.js
@@ -1,28 +1,49 @@
-const today = new Date()
+// // const today = new Date()
 
-console.log(today);
+// // console.log(today);
 
-let year = today.getFullYear()
+// // let year = today.getFullYear()
 
-console.log(year);
+// // console.log(year);
 
-let day = today.getDay()
+// // let day = today.getDay()
 
-console.log(day);
+// // console.log(day);
 
-// Challenge 1
-// Given a person's date of birth, create a function that calculates and returns their age in years.
+// // // Challenge 1
+// // // Given a person's date of birth, create a function that calculates and returns their age in years.
 
-let dob = "2003-07-07"
+// // let dob = "2003-07-07"
 
-let dobDate = new Date(dob)
-let dobYear = dobDate.getFullYear()
+// // let dobDate = new Date(dob)
+// // let dobYear = dobDate.getFullYear()
 
-let currentDate = new Date()
-let currYear = currentDate.getFullYear()
+// // let currentDate = new Date()
+// // let currYear = currentDate.getFullYear()
 
-let age = currYear - dobYear
+// // let age = currYear - dobYear
 
-console.log(age)
+// // console.log(age)
+
+
+// const today = new Date("2003-07-07")
+
+// console.log(today.getDay());
+
+
+// date between two dates
+
+const today = new Date("2003-07-07")
+
+today.setDate(today.getDate() + 10)
+
+console.log(today.toLocaleDateString());
+
+
+console.log(Date.now())
+
+let someDay = new Date(1730026632961)
+
+console.log(someDay);
 
 

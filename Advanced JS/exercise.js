76c3b74525bd0dd1afd904480421b80c8b3f6232
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-29 15:36:42 +1100

    exercise 4 dine

--- Advanced JS/exercise.js
+++ Advanced JS/exercise.js
@@ -85,5 +85,41 @@ const car = new Car(mazda)
 // console.log(car.displayDetails());
 
 
-// Write a JavaScript program that creates a class called 'Shape' with a method to calculate the area. Create two subclasses, 'Circle' and 'Triangle', that inherit from the 'Shape' class and override the area calculation method. Create an instance of the 
+// Write a JavaScript program that creates a class called 'Shape' with a method to calculate the area. Create two subclasses, 'Circle' and 'Triangle', that inherit from the 'Shape' class and override the area calculation method. Create an instance of the 'Circle' class and calculate its area. Similarly, do the same for the 'Triangle' class.
 
+class Shape{
+
+    calcArea(){
+        return 0
+    }
+}
+
+class Circle extends Shape{
+    constructor(radius){
+        super()
+        this.radius = radius
+    }
+
+    calcArea(){
+        return Math.PI * Math.pow(this.radius, 2)
+    }
+}
+
+class Triangle extends Shape{
+    constructor(base, height){
+        super()
+        this.base = base
+        this.height = height
+    }
+    
+    calcArea(){
+        return 0.5 * this.base * this.height
+    }
+}
+
+const circle1 = new Circle(5)
+const circle2 = new Triangle(10, 12)
+
+
+console.log(circle1.calcArea());
+console.log(circle2.calcArea());

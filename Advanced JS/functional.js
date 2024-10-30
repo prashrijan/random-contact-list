047cf1eec507a8d269cf6ec147672c758270fa13
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-30 23:57:49 +1100

    Higher Order Function

--- Advanced JS/functional.js
+++ Advanced JS/functional.js
@@ -14,10 +14,31 @@ const subtract = (a, b) => {
     return a - b
 }
 
+const multiplyBy2 = (val) => {
+    return val * 2
+}
+
 const calculate = (fn, x, y) => {
     return fn(x, y)
 }
 
-const result = calculate(add, 2, 3)
-console.log(result);
- 
+const result1 = calculate(add, 2, 3)
+const result2 = calculate(subtract, 2, 3)
+console.log(result1);
+console.log(result2);
+
+
+
+ const calc = (a, b) => {
+
+    const total = a + b
+
+    return multiply = (num) => {
+        return total * num
+    }
+
+ }
+
+ const ttl = calc(2, 3)(5)
+ console.log(ttl)
+

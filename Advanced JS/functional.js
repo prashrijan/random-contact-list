a2e398eb45f066334496103b5844144ed809ac4d
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-31 00:13:08 +1100

    funcitonal composition

--- Advanced JS/functional.js
+++ Advanced JS/functional.js
@@ -14,31 +14,36 @@ const subtract = (a, b) => {
     return a - b
 }
 
-const multiplyBy2 = (val) => {
-    return val * 2
+const multiply = (val, num) => {
+    return val * num
 }
 
-const calculate = (fn, x, y) => {
-    return fn(x, y)
+// const calculate = (fn, x, y) => {
+//     return fn(x, y)
+// }
+
+// const result1 = calculate(add, 2, 3)
+// const result2 = calculate(subtract, 2, 3)
+// console.log(result1);
+// console.log(result2);
+
+
+
+//  const calc = (a, b) => {
+
+//     const total = a + b
+
+//     return multiply = (num) => {
+//         return total * num
+//     }
+
+//  }
+
+//  const ttl = calc(2, 3)(5)
+//  console.log(ttl)
+
+const calc = (a, b, c) => {
+    return multiply(add(a, b), c)
 }
 
-const result1 = calculate(add, 2, 3)
-const result2 = calculate(subtract, 2, 3)
-console.log(result1);
-console.log(result2);
-
-
-
- const calc = (a, b) => {
-
-    const total = a + b
-
-    return multiply = (num) => {
-        return total * num
-    }
-
- }
-
- const ttl = calc(2, 3)(5)
- console.log(ttl)
-
+console.log(calc(2, 4, 2))

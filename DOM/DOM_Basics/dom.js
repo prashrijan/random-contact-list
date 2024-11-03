c18e89ab44421f081f7c2bdc7c4dcfab4da13ed4
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-03 13:11:22 +1100

    updating dom

--- DOM/DOM_Basics/dom.js
+++ DOM/DOM_Basics/dom.js
@@ -1,11 +1,27 @@
-const elemById = document.getElementById("first");
-const elemByClass = document.getElementsByClassName("info");
-const elemByTag = document.getElementsByTagName("div");
-const elemByQuerySelector = document.querySelector("div");
-const elemByQuerySelectorAll = document.querySelectorAll("div");
+// const elemById = document.getElementById("first");
+// const elemByClass = document.getElementsByClassName("info");
+// const elemByTag = document.getElementsByTagName("div");
+// const elemByQuerySelector = document.querySelector("div");
+// const elemByQuerySelectorAll = document.querySelectorAll("div");
 
-console.log(elemById);
-console.log(elemByClass[0]);
-console.log(elemByTag[1]);
-console.log(elemByQuerySelector);
-console.log(elemByQuerySelectorAll);
+// console.log(elemById);
+// console.log(elemByClass[0]);
+// console.log(elemByTag[1]);
+// console.log(elemByQuerySelector);
+// console.log(elemByQuerySelectorAll);
+
+// const info = document.querySelector(".info");
+// const infoVal = info.innerHTML;
+
+// console.log(info);
+// console.log(infoVal);
+
+// document.write("hey just adding more content");
+
+const elem = document.querySelector(".message");
+// const val = elem.innerText;
+
+// elem.innerText = "Hey just got text updated";
+elem.innerHTML = "<h1>Hey got response from the server</h1>";
+
+console.log(elem);

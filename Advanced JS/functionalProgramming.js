5e02b2926e8ed3422c28c0277641e38e02c5c1b1
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-04 10:23:38 +1100

    sum of array using recursion

--- Advanced JS/functionalProgramming.js
+++ Advanced JS/functionalProgramming.js
@@ -33,11 +33,65 @@
 // const add2 = (a, b) => a + b;
 
 // Higher Order Function
-const add = (a, b) => {
-  return a + b;
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const sub = (a, b) => {
+//   return a - b;
+// };
+
+// const calculator = (a, b, fnc) => {
+//     return fnc(a, b)
+// }
+
+// const val = calculator(2, 3, sub)
+// console.log(val);
+
+// const addAndSquare = (a, b) => {
+//   const total = a + b;
+
+//   const powerUp = (multiplier) => {
+//     return Math.pow(total, multiplier);
+//   };
+//   return powerUp;
+// };
+
+// const val = addAndSquare(2, 3)(2);
+
+// console.log(val);
+
+// const calculator = (a, b, calc) => {
+//   return calc(a, b);
+// };
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const sub = (a, b) => {
+//     return a - b
+// }
+
+// const val = calculator(2, 3, add)
+
+// console.log(val)
+
+// Recursion
+// debugger;
+// const factorial = (n) => {
+//   if (n <= 1) return 1;
+
+//   return n * factorial(n - 1);
+// };
+
+// console.log(`The factorial is ${factorial(3)}`);
+
+const money = [10, 20, 40, 40, 25];
+
+const total = (money) => {
+  if (money.length === 1) return money[0];
+  return money.pop() + total(money);
 };
 
-const sub = (a, b) => {
-  return a - b;
-};
-// Recursion
+console.log(total(money));
